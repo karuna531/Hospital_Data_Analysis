21a62d989ff44bf35e8c78be54f81c7ac9dcaaa3
Karuna Pandey <karunapandey125@gmail.com>
2026-07-17 17:12:20 +0545

    standarize gender

--- project.py
+++ project.py
@@ -57,20 +57,63 @@ diagnosis = diagnosis_data.drop_duplicates(subset=["DiagnosisID"], keep = "first
 doctor = doctor_data.drop_duplicates(subset=["DoctorID"], keep ="first")
 pharmacy = pharmacy_data.drop_duplicates(subset=["SaleID"], keep = "first")
 
+
+# standardarize 
+gender_map ={
+    'm': 'Male', 'M': 'Male', 'Mal':'Male', 'male': 'Male', 'Male':'Male', 
+    'f': 'Female', 'fmale': 'Female', 'female':'Female', 'femal':'Female', 'FeMale':'FeMale'
+}
+patient_data["Gender"] = patient_data["Gender"].astype(str).str.strip().replace(gender_map)
+print(department_data['Department'].unique())
+department_map ={
+    'endo': 'Endo', 'end':'Endo', 'edo':'Endo', 'Endo': 'Endo',
+    'pedo': 'Pedo', 'ped': 'Pedo', 'pedoo': 'Pedo', 'pdo': 'Pedo', 'Pedo': 'Pedo',
+    'ortho': 'Ortho', 'orth': 'Ortho', 'ort': 'Ortho', 'Ortho': 'Ortho',
+    'perio': 'Perio', 'Perio': 'Perio', 'Pero': 'Perio',
+    'Prostho': 'Prostho', 'prostho': 'Prostho', 
+    'dental': 'Dental', 'Dntal': 'Dental', 'den': 'Dental'
+}
+department_data["Department"] = department_data["Department"].astype(str).str.strip().replace(department_map)
+
+# remove leading and trailing space
+dataset = [
+    appointment_data,
+    billing_data,
+    department_data,
+    diagnosis_data,
+    doctor_data,
+    insurance_data,
+    patient_data,
+    pharmacy_data,
+    visit_data
+]
+for df in dataset:
+    obj = df.select_dtypes(include='string').columns
+    df[obj]=df[obj].apply(lambda x: x.str.strip())
+print("leading and trailing space is removed")
+
+#Convert dates into datetime format. 
+joining_date = pd.to_datetime( doctor_data["JoiningDate"], errors='coerce')
+print(joining_date.min())
 #replace null
 consultation_mean = billing_data["Consultation"].mean()
-print(consultation_mean)
 billing_data["Consultation"] = billing_data["Consultation"].fillna(consultation_mean)
 
 billing_data["Total"] =billing_data["Total"].fillna( billing_data["Lab"]- billing_data["Discount"] + billing_data["Tax"])
 billing_data["PaymentMethod"] = billing_data["PaymentMethod"].fillna(billing_data["PaymentMethod"].mode()[0])
 
-print("missing value", pd.isnull(billing_data).sum())
 
 department_data["Remarks"]= department_data["Remarks"].fillna("No Remarks")
-print("Missing Value", pd.isnull(department_data).sum())
 
-diagnosis_data["FollowUp"] =diagnosis_data["FollowUp"].fillna(Not Scheduled)
+diagnosis_data["FollowUp"] =diagnosis_data["FollowUp"].fillna("Not Scheduled")
 diagnosis_data["Notes"] = diagnosis_data["Notes"].fillna("Unknown")
-print(pd.isnull(diagnosis_data).sum())
-print(diagnosis_data["FollowUp"].head(),diagnosis_data["Notes"].head() )
+insurance_data["Provider"] = insurance_data["Provider"].fillna( insurance_data["Provider"].mode()[0])
+patient_data["BloodGroup"]= patient_data["BloodGroup"].fillna("unidentified")
+# mode = patient_data["Insurance"].mode()[0]
+patient_data["Insurance"]=patient_data["Insurance"].fillna("Unknown")
+patient_data["Age"]=patient_data["Age"].fillna(patient_data["Age"].mean())
+patient_data["Gender"] = patient_data["Gender"].fillna("unidentified")
+patient_data["District"] = patient_data["District"].fillna("Not Mentioned")
+pharmacy_data["PaymentMethod"]= pharmacy_data["PaymentMethod"].fillna(pharmacy_data["PaymentMethod"].mode()[0])
+
+
